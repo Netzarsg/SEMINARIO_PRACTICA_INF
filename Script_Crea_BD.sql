dbd3d7d534626c110ff5a555587b462cde382a4d
Netzarsg <58861017+Netzarsg@users.noreply.github.com>
2024-06-30 12:23:31 -0300

    Add files via upload

--- Script_Crea_BD.sql
+++ Script_Crea_BD.sql
@@ -1,95 +1,100 @@
-CREATE DATABASE ZPControlBD;
-USE ZPControlBd;
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Table Producto
+-- Schema mydb
 -- -----------------------------------------------------
-CREATE TABLE PRODUCTO (
-IdProducto INT NOT NULL PRIMARY KEY auto_increment,
-NombreProducto VARCHAR(100) NOT NULL
-);
-
 -- -----------------------------------------------------
--- Table Rol
+-- Schema controlprodbd
 -- -----------------------------------------------------
 
-CREATE TABLE ROL (
-IdRol INT NOT NULL PRIMARY KEY auto_increment,
-NombreRol VARCHAR(50) NOT NULL
-);
+-- -----------------------------------------------------
+-- Schema controlprodbd
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `controlprodbd` DEFAULT CHARACTER SET utf8mb4 ;
+USE `controlprodbd` ;
 
 -- -----------------------------------------------------
--- Table Usuario
+-- Table `controlprodbd`.`insumo`
 -- -----------------------------------------------------
-CREATE TABLE Usuario (
-IdUsuario INT PRIMARY KEY auto_increment,
-NombreUsuario VARCHAR(45) UNIQUE NOT NULL,
-Contraseña VARCHAR(45) NOT NULL,
-Estado VARCHAR(45) DEFAULT 'Activo',
-RolId INT NOT NULL,
-CONSTRAINT FK_Rol_Usuario FOREIGN KEY (RolID) REFERENCES ROL(IdRol)
-);
+CREATE TABLE IF NOT EXISTS `controlprodbd`.`insumo` (
+  `Id` INT(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`Id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 21
+DEFAULT CHARACTER SET = utf8mb4;
 
 
 -- -----------------------------------------------------
--- Table `ControlBD`.`OrdenTrabajo`
+-- Table `controlprodbd`.`ordentrabajo`
 -- -----------------------------------------------------
-
-CREATE TABLE OrdenTrabajo(
-IdOrdenTrabajo INT PRIMARY KEY auto_increment,
-NumeroOrden VARCHAR (45) UNIQUE NOT NULL,
-Fecha DATE NOT NULL,
-FechaCierra DATE NULL,
-Estado VARCHAR(45) DEFAULT 'Abierta',
-UsuarioId INT NOT NULL,
-ProductoId INT NOT NULL,
-CONSTRAINT FK_Usuario_OrdenTrabajo FOREIGN KEY(UsuarioId) REFERENCES Usuario(IdUsuario),
-CONSTRAINT FK_Producto_OrdenTrabajo FOREIGN KEY(ProductoId) REFERENCES Producto(IdProducto)
-);
-
-
--- -----------------------------------------------------
--- Table Tarea
--- -----------------------------------------------------
-
-CREATE TABLE TAREA (
-IdTarea INT PRIMARY KEY AUTO_INCREMENT,
-OrdenTarea INT NOT NULL,
-FabricacionProductoId INT NOT NULL,
-ProductoId INT NOT NULL,
-Descripcion VARCHAR(100) NOT NULL,
-FechaInicio DATE NULL,
-FechaFin DATE NULL,
-InsumoUno INT NULL,
-InsumoDos INT NULL,
-Observaciones VARCHAR(100) NULL,
-Estado VARCHAR(45) DEFAULT 'Pendiente',
-Usuario VARCHAR(45),
-CONSTRAINT FK_Producto_Tarea FOREIGN KEY (ProductoId) REFERENCES Producto(IdProducto)
-);
+CREATE TABLE IF NOT EXISTS `controlprodbd`.`ordentrabajo` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `NumeroOrden` INT(11) NOT NULL,
+  `Descripcion` VARCHAR(255) NOT NULL,
+  `FechaInicio` DATE NOT NULL,
+  `FechaFin` DATE NULL DEFAULT NULL,
+  `usuario` VARCHAR(50) NOT NULL,
+  `estado` VARCHAR(50) NOT NULL DEFAULT 'Abierta',
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 7
+DEFAULT CHARACTER SET = utf8mb4;
 
 
 -- -----------------------------------------------------
--- Table FabricacionProducto
+-- Table `controlprodbd`.`tarea`
 -- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `controlprodbd`.`tarea` (
+  `Id` INT(11) NOT NULL AUTO_INCREMENT,
+  `OrdenTrabajoId` INT(11) NOT NULL,
+  `NumeroOrdenTarea` INT(11) NOT NULL,
+  `Descripcion` VARCHAR(255) NOT NULL,
+  `FechaInicio` DATE NOT NULL,
+  `FechaCierre` DATE NULL DEFAULT NULL,
+  `Completada` TINYINT(1) NOT NULL,
+  `UsuarioResponsable` VARCHAR(50) NULL DEFAULT NULL,
+  `Insumo1Id` INT(11) NULL DEFAULT NULL,
+  `Insumo2Id` INT(11) NULL DEFAULT NULL,
+  `CantidadInsumo1` INT(11) NULL DEFAULT NULL,
+  `CantidadInsumo2` INT(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  INDEX `OrdenTrabajoId` (`OrdenTrabajoId` ASC) VISIBLE,
+  INDEX `Insumo1Id` (`Insumo1Id` ASC) VISIBLE,
+  INDEX `Insumo2Id` (`Insumo2Id` ASC) VISIBLE,
+  CONSTRAINT `tarea_ibfk_1`
+    FOREIGN KEY (`OrdenTrabajoId`)
+    REFERENCES `controlprodbd`.`ordentrabajo` (`id`),
+  CONSTRAINT `tarea_ibfk_2`
+    FOREIGN KEY (`Insumo1Id`)
+    REFERENCES `controlprodbd`.`insumo` (`Id`),
+  CONSTRAINT `tarea_ibfk_3`
+    FOREIGN KEY (`Insumo2Id`)
+    REFERENCES `controlprodbd`.`insumo` (`Id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 11
+DEFAULT CHARACTER SET = utf8mb4;
 
-CREATE TABLE FabricacionProducto(
-IdFabricacionProducto INT PRIMARY KEY AUTO_INCREMENT,
-OrdenId INT NOT NULL,
-CONSTRAINT FK_Orden_FabricacionProducto FOREIGN KEY (OrdenId) REFERENCES OrdenTrabajo(IdOrdenTrabajo)
-);
 
 -- -----------------------------------------------------
--- Table Documento
+-- Table `controlprodbd`.`usuario`
 -- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `controlprodbd`.`usuario` (
+  `Id` INT(11) NOT NULL AUTO_INCREMENT,
+  `nombreUsuario` VARCHAR(50) NOT NULL,
+  `contrasena` VARCHAR(50) NOT NULL,
+  `rol` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE INDEX `nombreUsuario` (`nombreUsuario` ASC) VISIBLE)
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8mb4;
 
-CREATE TABLE Documento(
-IdDocumento INT PRIMARY KEY AUTO_INCREMENT,
-Ruta VARCHAR(100) NOT NULL,
-FabricacionProductoId INT NOT NULL,
-CONSTRAINT FK_Fabricacion_Documento FOREIGN KEY (FabricacionProductoId) REFERENCES FabricacionProducto(IdFabricacionProducto)
-);
 
-
-
-
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
